eabbaea92e2c06202b53bd1e8f1c59de4b79db49
barat1378 <baratalihassanzada1378@gmail.com>
2023-09-17 23:20:14 +0330

    change saved

--- front/src/component/follow/FormBox.jsx
+++ front/src/component/follow/FormBox.jsx
@@ -2,11 +2,17 @@ import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import PersianFormBox from "./PersianFormBox";
 import EnglishFormBox from "./EnglishFormBox";
+import ChineseFormBox from "./ChineseFormBox";
 const FormBox = ({ setOpen, setCode }) => {
   const { language } = useSelector((state) => state.lan);
-  if (language == "Persian")
-    return <PersianFormBox setCode={setCode} setOpen={setOpen} />;
-  else return <EnglishFormBox setOpen={setOpen} setCode={setCode} />;
+  switch (language) {
+    case "Persian":
+      return <PersianFormBox setCode={setCode} setOpen={setOpen} />;
+    case "English":
+      return <EnglishFormBox setOpen={setOpen} setCode={setCode} />;
+    case "Chinese":
+      return <ChineseFormBox setOpen={setOpen} setCode={setCode} />;
+  }
 };
 
 export default FormBox;
